926ce3152a68343ea4e2af49707e7a1451c98296
irfan-f <irfan.filipovic23@gmail.com>
2020-07-20 16:13:49 -0700

    Updates

---
+++ precache-manifest.fa342e98a41fc120430f6caf0c8185e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d00fc46c0ec6173fde1e68ceac347728",
+    "url": "/portfolio-site/index.html"
+  },
+  {
+    "revision": "b05e5f0e81c20c99eec8",
+    "url": "/portfolio-site/static/css/main.adea832c.chunk.css"
+  },
+  {
+    "revision": "ffb7a41e4cdf4c102a1f",
+    "url": "/portfolio-site/static/js/2.63dd1328.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-site/static/js/2.63dd1328.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b05e5f0e81c20c99eec8",
+    "url": "/portfolio-site/static/js/main.0dc689be.chunk.js"
+  },
+  {
+    "revision": "74c5f092fc57aba5969a",
+    "url": "/portfolio-site/static/js/runtime-main.e0b85e7c.js"
+  }
+]);
