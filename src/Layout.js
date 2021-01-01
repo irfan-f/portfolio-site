b9ec4fd101c94583a72187456ab1eb1b39139e29
irfan-f <irfan.filipovic23@gmail.com>
2020-12-31 17:45:17 -0800

    Implemented state driven rendering for coursework component

--- src/Layout.js
+++ src/Layout.js
@@ -7,25 +7,25 @@ import Projects from './components/Projects';
 import './Layout.css'
 
 const navs = [
-  {name: '0', label: 'Home', link: "#home"},
-  {name: '1', label: 'Projects', link: "#projects"},
-  {name: '2', label: 'Course Work', link: "#courses"}
+  { name: '0', label: 'Home', link: "#home" },
+  { name: '1', label: 'Projects', link: "#projects" },
+  { name: '2', label: 'Course Work', link: "#courses" }
 ]
 
 function Layout() {
   return (
     <React.Fragment>
       <div className="layout-container">
-        <NavBar navs={navs}/>
-          <div className="layoutHome">
-            <Home></Home>
-          </div>
-          <div className="layout">
-            <Projects></Projects>
-          </div>
-          <div className="layout">
-            <CourseWork></CourseWork>
-          </div>
+        <NavBar navs={navs} />
+        <div className="layoutHome">
+          <Home></Home>
+        </div>
+        <div className="layout">
+          <Projects></Projects>
+        </div>
+        <div className="layout">
+          <CourseWork></CourseWork>
+        </div>
         <Footer></Footer>
       </div>
     </React.Fragment>
