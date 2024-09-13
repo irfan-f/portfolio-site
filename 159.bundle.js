67229d222f1b5eb361a5034873b3fc78a21e140b
irfan-f <irfan.filipovic23@gmail.com>
2024-09-12 19:52:31 -0700

    Updates

---
+++ 159.bundle.js
@@ -0,0 +1,1 @@
+(self.webpackChunkportfolio_app=self.webpackChunkportfolio_app||[]).push([[159],{159:(e,p,t)=>{var v=t(540);function a(e){return v.createElement("svg",e,v.createElement("path",{d:"M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z"}))}a.defaultProps={viewBox:"0 -960 960 960"},e.exports=a,a.default=a}}]);
