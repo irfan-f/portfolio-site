03d799e9424c11ceba17415d2115fe7c87e9ff6f
irfan-f <irfan.filipovic23@gmail.com>
2025-02-20 13:14:27 -0800

    remove FAB and modal incorporations, remove Draggable as only use was FAB

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,8 @@ import './index.css';
 import App from './App';
 import { HashRouter as Router } from 'react-router-dom';
 import { IconContext } from 'react-icons';
-import FAB from './components/interactive/FABs/ScrollToTop';
-import { createPortal } from 'react-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
-const fabRoot = document.getElementById('fab-root');
 
 const RootComponent = () => {
   useEffect(() => {
@@ -21,9 +18,6 @@ const RootComponent = () => {
 
   return (
     <React.StrictMode>
-      { fabRoot &&
-        createPortal(<FAB />, fabRoot)
-      }
       <Router basename={window.location.pathname || ''}>
         <IconContext.Provider value={{ className: 'react-icons' }}>
           <App />
